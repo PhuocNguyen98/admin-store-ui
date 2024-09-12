7913a0f50a83efb313df557f583fd2d842b30614
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-12 14:53:05 +0700

    Chỉnh sửa hiển thị dữ liệu TableData và Cập nhật cho phép chỉnh sửa trên table( html select)

--- src/components/TableStyle/TableBodyStyle.js
+++ src/components/TableStyle/TableBodyStyle.js
@@ -1,6 +1,8 @@
 import Chip from '@mui/material/Chip';
+import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import TableRow from '@mui/material/TableRow';
+import MenuItem from '@mui/material/MenuItem';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -10,6 +12,8 @@ import { Link } from 'react-router-dom';
 import images from '~/assets/img';
 import classnames from 'classnames/bind';
 import styles from './TableStyle.module.scss';
+
+import { Controller } from 'react-hook-form';
 
 const cx = classnames.bind(styles);
 
@@ -38,74 +42,131 @@ const theme = createTheme({
         },
       },
     },
+    MuiSelect: {
+      styleOverrides: {
+        root: {
+          fontSize: '1.4rem',
+        },
+      },
+    },
+    MuiMenuItem: { styleOverrides: { root: { fontSize: '1.4rem' } } },
   },
 });
 
-const onErrorImg = (e) => {
-  e.target.src = images.imgPlacehoder;
-};
+function TableBodyStyle({ columns, tableData, actions, control, setValue }) {
+  const onErrorImg = (e) => {
+    e.target.src = images.imgPlacehoder;
+  };
 
-function TableBodyStyle({ columns, tableData, actions }) {
+  const renderBasic = (column, data, id, value, onChange) => {
+    switch (column?.component) {
+      case 'image': {
+        return (
+          <TableCell key={column.accessor}>
+            <img
+              src={`${data}`}
+              alt=''
+              style={{
+                width: '100px',
+                height: '100px',
+                objectFit: 'cover',
+              }}
+              onError={(e) => onErrorImg(e)}
+            />
+          </TableCell>
+        );
+      }
+      case 'chip': {
+        return (
+          <TableCell key={column.accessor}>
+            {column.displayType.map((col) => {
+              if (col.value === data) {
+                return (
+                  <Chip
+                    key={col.value}
+                    label={col.title}
+                    color={col.color}
+                    variant='outlined'
+                  />
+                );
+              }
+              return '';
+            })}
+          </TableCell>
+        );
+      }
+      case 'select': {
+        return (
+          <TableCell key={column.accessor}>
+            <Select value={value} onChange={onChange}>
+              {column.displayType.map((col) => (
+                <MenuItem key={col.value} value={col.value}>
+                  {col.title}
+                </MenuItem>
+              ))}
+            </Select>
+          </TableCell>
+        );
+      }
+      case 'actions': {
+        return (
+          <TableCell key={column.accessor}>
+            {actions.map((action, index) => (
+              <Link
+                key={index}
+                to={action.to ? `${action.to}/${id}` : null}
+                className={cx('action-link')}
+              >
+                <Button
+                  component='div'
+                  variant='contained'
+                  startIcon={action.icon}
+                  onClick={action?.onClick ? action.onClick : null}
+                  {...action?.css}
+                >
+                  {action.title}
+                </Button>
+              </Link>
+            ))}
+          </TableCell>
+        );
+      }
+      default:
+        const cellData = data ? data : ''; // render data text basic
+        return <TableCell key={column.accessor}>{cellData}</TableCell>;
+    }
+  };
+
+  const renderController = (column, data, name, id) => {
+    let accessor = column.accessor;
+    setValue(`formList.${name}.${accessor}`, data);
+    return (
+      <Controller
+        key={column.accessor}
+        control={control}
+        name={name}
+        render={({ field: { value, onChange } }) =>
+          renderBasic(column, data, id, value, onChange)
+        }
+      />
+    );
+  };
+
   const renderTableBody = () =>
     tableData.map((data, index) => (
       <TableRow key={data.id}>
         {columns.map((col) => {
-          if (col.accessor === 'thumbnail') {
-            return (
-              <TableCell key={col.accessor}>
-                <img
-                  src={`${data[col.accessor]}`}
-                  alt=''
-                  style={{
-                    width: '100px',
-                    height: '100px',
-                    objectFit: 'cover',
-                  }}
-                  onError={(e) => onErrorImg(e)}
-                />
-              </TableCell>
-            );
+          if (col?.component && !col?.editTable) {
+            return renderBasic(col, data[col.accessor], data.id);
+          } else {
+            let accessor = col.accessor;
+            let name = `formList.${index}.${accessor}`;
+            if (typeof setValue === 'function') {
+              setValue(`formList.${index}.${accessor}`, data[col.accessor]);
+              setValue(`formList.${index}.id`, data.id);
+            }
+            return renderController(col, data[col.accessor], name, data.id);
           }
-          if (col.accessor.includes('is_')) {
-            return (
-              <TableCell key={col.accessor}>
-                <Chip
-                  label={
-                    data[col.accessor]
-                      ? col.displayType[0].title
-                      : col.displayType[1].title
-                  }
-                  color={data[col.accessor] ? 'success' : 'primary'}
-                  variant='outlined'
-                />
-              </TableCell>
-            );
-          }
-          if (col.accessor === 'actions') {
-            return (
-              <TableCell key={col.accessor}>
-                {actions.map((action, index) => (
-                  <Link
-                    key={index}
-                    to={action.to ? `${action.to}/${data.id}` : null}
-                    className={cx('action-link')}
-                  >
-                    <Button
-                      component='div'
-                      variant='contained'
-                      startIcon={action.icon}
-                      onClick={action?.onClick ? action.onClick : null}
-                      {...action?.css}
-                    >
-                      {action.title}
-                    </Button>
-                  </Link>
-                ))}
-              </TableCell>
-            );
-          }
-          const cellData = data[col.accessor] ? data[col.accessor] : '';
-          return <TableCell key={col.accessor}>{cellData}</TableCell>;
         })}
       </TableRow>
     ));
