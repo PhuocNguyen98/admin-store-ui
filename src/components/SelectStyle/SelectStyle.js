f0e85debf4b4176abb0797c3f2978eeb1b3939d1
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-13 22:05:38 +0700

    Thêm hiển thị lỗi cho component SelectStyle MUI

--- src/components/SelectStyle/SelectStyle.js
+++ src/components/SelectStyle/SelectStyle.js
@@ -1,6 +1,7 @@
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
 import { ThemeProvider, createTheme } from '@mui/material';
 
 import { Controller } from 'react-hook-form';
@@ -21,28 +22,48 @@ const themeSelect = createTheme({
         },
       },
     },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: {
+          fontSize: '1.1rem',
+        },
+      },
+    },
   },
 });
 
-function SelectStyle({ control, name, options }) {
+function SelectStyle({ control, name, options, title, ...props }) {
   return (
     <ThemeProvider theme={themeSelect}>
       <Controller
         control={control}
         name={name}
-        render={({ field: { value, onChange } }) => (
+        render={({ field: { value, onChange }, fieldState: { error } }) => (
           <FormControl fullWidth>
-            <Select onChange={onChange} value={value}>
-              {options.length > 0
-                ? options.map((item, index) => {
-                    return (
-                      <MenuItem key={index} value={item.value}>
-                        {item.title}
-                      </MenuItem>
-                    );
-                  })
-                : null}
+            <Select
+              onChange={onChange}
+              value={value}
+              error={!!error}
+              {...props}
+            >
+              <MenuItem disabled>{title}</MenuItem>
+              {options.length > 0 ? (
+                options.map((item) => {
+                  return (
+                    <MenuItem key={item.value} value={item.value}>
+                      {item.title}
+                    </MenuItem>
+                  );
+                })
+              ) : (
+                <span
+                  style={{ display: 'block', textAlign: 'center', padding: 10 }}
+                >
+                  No data available
+                </span>
+              )}
             </Select>
+            <FormHelperText error={!!error}>{error?.message}</FormHelperText>
           </FormControl>
         )}
       />
