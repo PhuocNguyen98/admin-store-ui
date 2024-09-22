f07fe95211a6dea02e3d3ccea66ebc7e8ada8945
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-22 09:46:44 +0700

    Cập nhật file store và xóa các file không cần thiết

--- src/store/actionsType/staffRoleActions.js
+++
@@ -1,6 +0,0 @@
-export const GET_STAFF_ROLE = 'GET_STAFF_ROLE';
-
-export const fetchDataSuccess = (data) => ({
-  type: GET_STAFF_ROLE,
-  payload: data,
-});
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
-// import customerReducer from './reducers/customerReducer';
-import staffReducer from './reducers/staffReducer';
-import staffRoleReducer from './reducers/staffRoleReducer';
 
-const reducer = combineReducers({
-  // customer: customerReducer,
-  staff: staffReducer,
-  role: staffRoleReducer,
-});
+const reducer = combineReducers({});
 
 const store = createStore(reducer, applyMiddleware(thunk));
 export default store;
