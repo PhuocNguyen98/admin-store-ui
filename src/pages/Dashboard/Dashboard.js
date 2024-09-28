f225eefa458fdb2f282042328ac00ebba4ff3e91
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-09-28 09:30:46 +0700

    Cập nhật giao diện dashbroad đơn giản

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
+import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import { useSelector } from 'react-redux';
+
 function Dashboard() {
+  const user = useSelector((state) => state.user);
+
   return (
     <div>
-      <h2>Dashboard</h2>
+      <em style={{ lineHeight: '1.6rem' }}>
+        Welcome, {Object.keys(user.data).length > 0 ? user.data?.username : ''}. Have a nice day{' '}
+        <SentimentSatisfiedAltIcon />!
+      </em>
     </div>
   );
 }
